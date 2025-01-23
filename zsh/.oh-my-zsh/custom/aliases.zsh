12a572bcc4dffd305838466b6d6a2e8bfdfcbcd2
Harry Terkelsen <het@google.com>
2025-01-23 10:00:15 -0800

    Remove comments from zshrc

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,1 +1,13 @@
+alias vim=nvim
+alias s="kitten ssh"
 alias unstow='stow --delete'
+
+## Eza aliases
+alias l='eza --hyperlink'
+alias ll='eza -lbF --git --hyperlink'
+alias la='eza -lbhHigmuSa --time-style=long-iso --git --color-scale --hyperlink'
+alias lx='eza -lbhHigmuSa@ --time-style=long-iso --git --color-scale --hyperlink'
+alias llt='eza -l --git --tree --hyperlink'
+alias lt='eza --tree --level=2 --hyperlink'
+alias llm='eza -lbGF --git --sort=modified --hyperlink'
+alias lld='eza -lbhHFGmuSa --group-directories-first --hyperlink'

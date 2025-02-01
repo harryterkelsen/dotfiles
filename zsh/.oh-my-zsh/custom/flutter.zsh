6ec534fedc549916f568aec9b5acfb0b4d80c5ae
Harry Terkelsen <harry.terkelsen@gmail.com>
2025-02-01 14:58:42 -0800

    Use google-chrome-stable

--- zsh/.oh-my-zsh/custom/flutter.zsh
+++ zsh/.oh-my-zsh/custom/flutter.zsh
@@ -3,5 +3,5 @@ export PATH="$HOME/Projects/depot_tools:$HOME/Projects/flutter/bin:$PATH"
 if [[ $(uname) == "Darwin" ]]; then
   export CHROME_EXECUTABLE="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 else
-  export CHROME_EXECUTABLE="google-chrome"
+  export CHROME_EXECUTABLE="google-chrome-stable"
 fi

0ef9d5f11be8a5b60c9857d880def95431aa1e4f
Harry Terkelsen <harry.terkelsen@gmail.com>
2025-02-12 10:22:36 -0800

    Add pub bin

--- zsh/.oh-my-zsh/custom/flutter.zsh
+++ zsh/.oh-my-zsh/custom/flutter.zsh
@@ -1,4 +1,4 @@
-export PATH="$HOME/Projects/depot_tools:$HOME/Projects/flutter/bin:$PATH"
+export PATH="$HOME/Projects/depot_tools:$HOME/Projects/flutter/bin:$HOME/.pub-cache/bin:$PATH"
 
 if [[ $(uname) == "Darwin" ]]; then
   export CHROME_EXECUTABLE="/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
